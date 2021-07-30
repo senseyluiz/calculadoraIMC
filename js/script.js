5cc6b198323f993663775815707438dd6edd777d
Senseyluiz <senseyluiz@gmail.com>
2021-07-30 10:36:58 -0300

    Correção do bug do IF

--- js/script.js
+++ js/script.js
@@ -8,26 +8,28 @@ function calcular(){
     resultado.innerHTML = imc.toFixed(2);
 
     let resposta = document.getElementById("resposta");
-    
-    if(imc < 18,5){
-        resposta.innerHTML = "<p>Você está abaixo do peso.</p>"
-    }
+    console.log(Number.isNaN(imc));
 
-    if(imc >= 18,5 && imc < 25){
-        resposta.innerHTML = "<p>Você está com peso normal!</p>"
+    if(Number.isNaN(imc) === true){
+        resposta.innerHTML = "<p> Por favor, preencha todos os dados.</p>"
     }
-
-    if(imc >= 25 && imc < 30){
-        resposta.innerHTML = "<p>Você está com sobrepeso!</p>"
+    if(imc < 18.5){
+        resposta.innerHTML = "<p> Você está abaixo do peso.</p>"
     }
-
-    if(imc >= 30){
-        resposta.innerHTML = "<p>Você está com obesidade. CUIDADO!!!</p>"
+    if(imc > 18.5 && imc < 25){
+        resposta.innerHTML = "<p> Você está com peso normal. Parabéns !!!</p>"
     }
-    
+    if(imc > 25 && imc < 30){
+        resposta.innerHTML = "<p> Você está com sobrepeso. Fique atento!</p>"
+    }
+    if(imc > 30){
+        resposta.innerHTML = "<p> Você está com obesidade. CUIDADO!!!</p>"
+    }
+        
 }
 
 function limpar(){
+    document.getElementById("resposta").innerHTML = "";
     document.getElementById("altura").value = "";
     document.getElementById("peso").value = "";
     document.getElementById("imc").innerHTML= "";
